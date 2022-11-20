b8ee70e4dff308f87c5c9a303b7221f618d54cf5
Son-Draken <117678148+Son-Draken@users.noreply.github.com>
2022-11-20 18:21:54 +0100

    Add files via upload

---
+++ 2priklad_Lekce17_18_OOP/2priklad_Lekce17_18_OOP/MujList.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2priklad_Lekce17_18_OOP
+{
+    internal class MujList
+    {
+
+        
+        private object [] pole = new object[10];
+
+        int Pocet { get; set; }
+
+
+        public object this[int i]
+        {
+            get { return pole[i]; }
+            set { pole[i] = value; }
+        }
+
+        public void Pridej(object cislo)
+        {
+            pole[Pocet] = cislo;
+            Pocet++;
+           if (Pocet == pole.Length)
+            {
+                object[] delsiPole = new object[Pocet*2];
+                pole.CopyTo(delsiPole, 0);
+                pole = delsiPole;
+            }
+
+        }
+
+        //public override string ToString()
+        //{
+        //    return Pocet;
+        //}
+
+        public void vypiš()
+        {
+            foreach (object i in pole)
+                Console.WriteLine(i);
+        }
+
+    }
+}
---
+++ 2priklad_Lekce17_18_OOP/2priklad_Lekce17_18_OOP/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2priklad_Lekce17_18_OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         
+           MujList mujList = new MujList();
+
+            mujList.Pridej(1);
+            mujList.Pridej("osm");
+            mujList.Pridej(3);
+            mujList.Pridej(4);
+
+         for (int i = 0; i < 100; i++)
+            {
+                mujList.Pridej(i);
+            }
+
+         mujList[5] = "deset";
+
+           
+            for( int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(mujList[i]);
+            }
+
+
+
+
+        }
+    }
+}
