6197036b72e3c1923df5d204230b39672a63539b
Draken <kovis12@seznam.cz>
2022-11-20 17:50:36 +0100

    prvni

---
+++ 1priklad_Lekce_17_18_OOP/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1priklad_Lekce_17_18_OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Uzivatel> list = new List<Uzivatel>();
+
+            list.Add(new Uzivatel("Karel", "Novák", new DateTime(1988, 02, 05), Uzivatel.Pohlavi.Muz, Uzivatel.BarvaOci.Zelena, Uzivatel.BarvaVlasu.Blond));
+            list.Add(new Uzivatel("Josef", "Nový", new DateTime(1978, 03, 19), Uzivatel.Pohlavi.Muz, Uzivatel.BarvaOci.Hneda, Uzivatel.BarvaVlasu.Hnede));
+            list.Add(new Uzivatel("Jan", "Marek", new DateTime(1968, 04, 28), Uzivatel.Pohlavi.Muz, Uzivatel.BarvaOci.Cerna, Uzivatel.BarvaVlasu.Neuvedeno));
+            list.Add(new Uzivatel("Karel", "Novák", new DateTime(1988, 02, 05), Uzivatel.Pohlavi.Muz, Uzivatel.BarvaOci.Zelena, Uzivatel.BarvaVlasu.Blond));
+            list.Add(new Uzivatel("Marie", "Nová", new DateTime(1988, 08, 15), Uzivatel.Pohlavi.Zena, Uzivatel.BarvaOci.Modra, Uzivatel.BarvaVlasu.Cerna));
+            list.Add(new Uzivatel("Věra", "Nováková", new DateTime(1978, 10, 2), Uzivatel.Pohlavi.Zena, Uzivatel.BarvaOci.Modrozelena, Uzivatel.BarvaVlasu.Blond));
+            list.Add(new Uzivatel("Simona", "Mladá", new DateTime(1968, 01, 08), Uzivatel.Pohlavi.Zena, Uzivatel.BarvaOci.Neuvedeno, Uzivatel.BarvaVlasu.Hnede));
+            list.Add(new Uzivatel("Michaela", "Marná", new DateTime(1958, 12, 06), Uzivatel.Pohlavi.Zena, Uzivatel.BarvaOci.Modra, Uzivatel.BarvaVlasu.Zrzava));
+
+
+
+            //foreach (Uzivatel c in list)
+            //    Console.WriteLine(c);
+
+            //Console.WriteLine("");
+
+            //List<Uzivatel> list2 = list.Distinct().ToList();
+
+            //foreach (Uzivatel b in list2)
+            //    Console.WriteLine(b);
+
+            Console.WriteLine(" ");
+            list = list.Distinct().ToList();
+
+            foreach (Uzivatel c in list)
+                Console.WriteLine(c);
+
+            Console.WriteLine("hotovo");
+        }
+    }
+}
---
+++ 1priklad_Lekce_17_18_OOP/Uzivatel.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1priklad_Lekce_17_18_OOP
+{
+    internal class Uzivatel
+    {
+        public string Jmeno { get; set; }
+        public string Prijmeni { get; set; }
+        public DateTime DatumNarozeni { get; set; }
+        public enum Pohlavi { Muz, Zena }
+        public Pohlavi Pohlav { get; set; }
+        public BarvaOci Oci { get; set; }
+        public BarvaVlasu Vlasy { get; set; }
+
+        [Flags]
+        public enum BarvaOci
+        {
+            Neuvedeno = 0,
+            Modra = 1,
+            Zelena = 2,
+            Hneda = 4,
+            Cerna = 8,
+            Modrozelena = 16,
+            Hnedozelena = 32,
+        }
+
+        public enum BarvaVlasu { Neuvedeno, Blond, Hnede, Cerna, Zrzava }
+
+        public Uzivatel(string jmeno, string prijmeni, DateTime datumNarozeni, Pohlavi pohlavi, BarvaOci barvaOci, BarvaVlasu barvaVlasu)
+        {
+            Jmeno = jmeno;
+            Prijmeni = prijmeni;
+            DatumNarozeni = datumNarozeni;
+            Pohlav = pohlavi;
+            Oci = barvaOci;
+            Vlasy = barvaVlasu;
+
+        }
+
+        public override bool Equals(object obj)
+        {
+            return ((obj is Uzivatel) && (this == (Uzivatel)obj));
+        }
+
+        public static bool operator ==(Uzivatel a, Uzivatel b)
+        {
+            return ((a.Jmeno == b.Jmeno) && 
+                (a.Prijmeni == b.Prijmeni) && 
+                (a.DatumNarozeni == b.DatumNarozeni) && 
+                (a.Pohlav == b.Pohlav) && 
+                (a.Oci == b.Oci) && 
+                (a.Vlasy == b.Vlasy));
+        }
+        public static bool operator !=(Uzivatel a, Uzivatel b)
+        {
+            return !(a == b);
+        }
+
+        public override int GetHashCode()
+        {
+            return Jmeno.GetHashCode() ^ Prijmeni.GetHashCode() ^ DatumNarozeni.GetHashCode() ^ Pohlav.GetHashCode() ^ Oci.GetHashCode() ^ Vlasy.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0} {1}, {2}, oči: {3}, vlasy: {4}", Jmeno, Prijmeni, DatumNarozeni.Year, Oci, Vlasy);
+            
+        }
+
+     
+        }
+    }
+
+
+
+
+
+
+
+
+
